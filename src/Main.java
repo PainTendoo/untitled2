0b8a51ab4cd627bb74868dc4eea9f36b1c6e8794
Pain <vadimklimkin1@gmail.com>
2023-01-20 16:27:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1000000;
+        int percent = 2000;
+        int bonus = amount / percent;
+        System.out.println("Vadim, hello! Thank you for choosing our airline for your flight. Your bonus is:"+ bonus +" miles");
+    }
+}
